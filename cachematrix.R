9f70fa8842ef80d38b484fd85708aa9754f11345
Nikhil <nikhilsable@nikhils-MacBook-Pro.local>
2016-11-05 08:32:10 -0400

    Updated cache matrix.R function
    Updated cache matrix.R function for Coursera R programming assignment -
    Nikhil Sable

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix() gets a square matrix (x) and 
+## generates a list (to set/get matrix, and setmatrix/getmatrix its inverse)
+## which acts as the input to cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mt<-NULL
+  set<-function(y){
+    x<<-y
+    mt<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) mt<<- solve
+  getmatrix<-function() mt
+  list(set=set, get=get,setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## for cacheSolve the input x is the outut from makeCacheMatrix (the list)
+## The function returns the inverse(mt) of the original matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mt<-x$getmatrix()
+  # below if{} statement checks whether mt(inverse matrix) has been 
+  ## calculated and is not null. If TRUE, returns the cached inversed matrix(mt).
+  if(!is.null(mt)){
+    message("getting cached data")
+    return(mt)
+  }
+  matrix<-x$get()
+  ## below statement is where we are solving(inverse) for original matrix x
+  mt<-solve(matrix, ...)
+  x$setmatrix(mt)
+  mt
 }
